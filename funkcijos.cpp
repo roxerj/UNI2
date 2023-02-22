dbf658ae7dcfdaabdf61ee4e6c5057bb1abe76c7
roxerj <32255747+roxerj@users.noreply.github.com>
2023-02-22 20:26:34 +0200

    funkcijos.cpp

---
+++ funkcijos.cpp
@@ -0,0 +1,66 @@
+#include <iomanip>
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include "funkcijos.h"
+#include <chrono>
+#include <fstream>
+
+void print(std::vector<studentas> &studentai)
+{
+    std::string output = "Vardas         Pavarde           Galutinis(vid.) Galutinis(med.)\n";
+    output += "-----------------------------------------------------------------------\n";
+    
+    auto start2 = std::chrono::high_resolution_clock::now();
+    for (auto &studentas : studentai)
+    {
+        char a[20];
+        sprintf(a, "%5.2f           %5.2f", studentas.galutinis_vid, studentas.galutinis_med);
+
+        studentas.vardas.resize(15, ' ');
+        studentas.pavarde.resize(17, ' ');
+
+        output += studentas.vardas + studentas.pavarde + std::string(a) + "\n";
+    }
+    auto end2 = std::chrono::high_resolution_clock::now();
+    
+    
+    auto start3 = std::chrono::high_resolution_clock::now();
+    std::ofstream out_file("output.txt");
+    out_file << output;
+    out_file.close();
+    auto end3 = std::chrono::high_resolution_clock::now();
+
+    std::chrono::duration<double> duration2 = end2 - start2;
+    std::chrono::duration<double> duration3 = end3 - start3;
+    std::cout << "\nOutputo formatavimas uztruko " << duration2.count() << " sec." << std::endl;
+    std::cout << "Outputo isvedimas i faila uztruko " << duration3.count() << " sec." << std::endl;
+
+}
+
+
+double count_med(std::vector<int> &nd_vec)
+{
+    std::sort(nd_vec.begin(), nd_vec.end());
+    if(nd_vec.size() % 2 != 0)
+    {
+        return nd_vec.at((nd_vec.size() - 1) / 2);
+    }
+    else
+    {
+        return (double(nd_vec.at((nd_vec.size()/2) - 1)) + double(nd_vec.at(nd_vec.size()/2))) / 2;
+    }
+}
+
+double count_vid(const std::vector<int> &nd_vec)
+{
+    double temp = 0;
+    for(auto pazymys:nd_vec)
+    {
+        temp += pazymys;
+    }
+    return temp/double(nd_vec.size());
+}
+
+
