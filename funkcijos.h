92153133f4d8f7a1522dfc04625a467f8ee64706
roxerj <32255747+roxerj@users.noreply.github.com>
2023-02-22 20:26:08 +0200

    funkcijos.h

---
+++ funkcijos.h
@@ -0,0 +1,24 @@
+
+#ifndef FUNKCIJOS_H_
+#define FUNKCIJOS_H_
+#include <vector>
+#include <string>
+
+struct studentas {
+    std::string vardas;
+    std::string pavarde;
+    std::vector<int> nd_vec {};
+    int egz {};
+    float galutinis_vid {};
+    float galutinis_med {};
+};
+
+
+void print(std::vector<studentas> &studentai);
+
+double count_med(std::vector<int> &nd_vec);
+
+double count_vid(const std::vector<int> &nd_vec);
+
+
+#endif
