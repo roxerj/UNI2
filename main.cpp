edc76f3d95ec5447db03eddbc3314d552bfc138d
roxerj <32255747+roxerj@users.noreply.github.com>
2023-03-05 11:48:54 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,88 +5,134 @@
 #include <fstream>
 #include <chrono>
 #include <stdexcept>
+#include <filesystem>
+#include <limits>
 
 int main(){
     
+    int choice, studentu_skaicius;
+    std::string testas, failo_pavadinimas;
 
-    // system("dir");
-    // system("pause");
-    std::string failas;
-    std::ifstream in_file;
-
+    
     while (true) 
     {
-        std::cout << "Iveskite failo pavadinima: ";
-        std::cin >> failas;
+        std::cout << "Iveskite 1, jei norite generuoti studentu faila, iveskite 2, jei norite atlikti veiksmus su esamu failu, arba 3, jei norite iseiti: ";
+        std::cin >> choice;
+
         try 
         {
-            in_file.open(failas);
-            if (!in_file) 
+            if (std::cin.fail()) 
             {
-                throw std::runtime_error("Failo atidaryti nepavyko.");
+                throw std::runtime_error("Klaida: blogas pasirinkimas. Pasirinkite 1, 2 arba 3");
             }
-            std::cout << "Failas atidarytas sekmingai" << std::endl;
-            break;
-        } catch (const std::exception& e) 
+
+            if (choice == 1) 
+            {
+                std::cout << "Kiek bus studentu? ";
+                std::cin >> studentu_skaicius;
+
+                try {
+                    if (std::cin.fail() || studentu_skaicius <= 0) 
+                    {
+                        throw std::runtime_error("Klaida: neteisingai ivestas studentu skaicius");
+                    }
+
+                    testas = generuoti(studentu_skaicius);
+                    failo_pavadinimas = "stud" + std::to_string(studentu_skaicius) + ".txt";
+
+                    std::ofstream out_file(failo_pavadinimas);
+
+                    if (!out_file.is_open()) 
+                    {
+                        throw std::runtime_error("Klaida: nepavyko sukurti failo");
+                    }
+
+                    out_file << testas;
+                    out_file.close();
+                }
+                catch (const std::runtime_error& e) 
+                {
+                    std::cout << e.what() << std::endl;
+                    std::cin.clear();
+                    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+                }
+            }
+
+            else if (choice == 2) 
+            {
+                std::cout << "Esami failai:\n";
+
+                for (const auto& entry : std::filesystem::directory_iterator(".")) {
+                    if (entry.is_regular_file()) {
+                        std::cout << entry.path().filename() << std::endl;
+                    }
+                }
+
+
+                std::cout << "Iveskite failo pavadinima: ";
+                std::cin >> failo_pavadinimas;
+
+                std::ifstream in_file(failo_pavadinimas);
+
+                if (!in_file) 
+                {
+                    throw std::runtime_error("Klaida: failas " + failo_pavadinimas + " neegzistuoja");
+                }
+
+                in_file.close();
+                break;
+            }
+
+            else if (choice == 3) 
+            {
+                std::cout << "Programa baige darba" << std::endl;
+                return 0;
+            }
+
+            else 
+            {
+                throw std::runtime_error("Klaida: blogas pasirinkimas. Pasirinkite 1, 2 arba 3");
+            }
+        }
+        catch (const std::runtime_error& e) 
         {
-            std::cerr << e.what() << std::endl;
+            std::cout << e.what() << std::endl;
+            std::cin.clear();
+            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
         }
     }
+
+    
+    
+    Timer bendras_timer;
+    bendras_timer.start();
 
     std::vector<studentas> studentai;
-    studentai.reserve(1000000);
-    int temp_nd, nd_count, word_count = 0;
-    std::string line, word;
-    getline(in_file, line);
- 
-    std::istringstream iss(line);
-    while(iss >> word)
-    {
-        word_count++;
-    }
-    nd_count = word_count - 3;
-    std::cout << "ND skaicius: " << nd_count << std::endl;
-
-    auto start1 = std::chrono::high_resolution_clock::now(); //start timing
-    std::string temp_vardas, temp_pavarde;
     
-    while(in_file >> temp_vardas >> temp_pavarde)
-    {
-        studentas stud;
-        stud.vardas = temp_vardas;
-        stud.pavarde = temp_pavarde;
-        for(int i = 0; i < nd_count; i++)
-        { 
-            in_file >> temp_nd;
-            stud.nd_vec.push_back(temp_nd);
-            temp_nd = 0;
-        }
-        in_file >> stud.egz;
-        
-        stud.galutinis_med = 0.4 * count_med(stud.nd_vec) + 0.6 * stud.egz;
-        stud.galutinis_vid = 0.4 * count_vid(stud.nd_vec) + 0.6 * stud.egz;
+    Timer nuskaitymas_timer;
+    nuskaitymas_timer.start();
+    nuskaitymas(studentai, failo_pavadinimas);
+    nuskaitymas_timer.stop();
+    std::cout << "Duomenu nuskaitymas ir galutiniu pazymiu skaiciavimas uztruko " << nuskaitymas_timer.elapsed() << " sec." << std::endl;
 
-        studentai.push_back(stud);
-        
-    }
-
+    Timer sort_timer;
+    sort_timer.start();
+    std::vector<studentas> vargsai;
+    std::vector<studentas> malaciai;
+    rusiavimas(studentai, vargsai, malaciai);
+    sort_timer.stop();
+    std::cout << "Studentu rusiavimas i vargsus ir malacius uztruko " << sort_timer.elapsed() << " sec." << std::endl;
     
-    auto end1 = std::chrono::high_resolution_clock::now(); // stop timing
-    std::chrono::duration<double> duration1 = end1 - start1;
-    std::cout << "Duomenu nuskaitymas ir galutiniu pazymiu skaiciavimas uztruko " << duration1.count() << " sec." << std::endl;
 
-    auto start_sort = std::chrono::high_resolution_clock::now(); //start timing
-    std::sort(studentai.begin(), studentai.end(), compare);
-    auto end_sort = std::chrono::high_resolution_clock::now(); // stop timing
-    std::chrono::duration<double> duration_sort = end_sort - start_sort;
-    std::cout << "Studentu rusiavimas pagal varda/pavarde uztruko " << duration_sort.count() << " sec." << std::endl;
+    isvedimas(vargsai, "vargsai.txt");
+    isvedimas(malaciai, "malaciai.txt");
 
-    print(studentai);
+    bendras_timer.stop();
+    std::cout << "Visos programos veikimo laikas " << bendras_timer.elapsed() << " sec." << std::endl;
+    std::cout << "Malaciu skaicius: " << malaciai.size() << ", vargsu skaicius: " << vargsai.size();
 
     
 
-    studentai.clear();
-    in_file.close();
     return 0;
 }
 
