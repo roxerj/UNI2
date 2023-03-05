f18e997e9e793767f4b9f96e1a3bf53309c848b3
roxerj <32255747+roxerj@users.noreply.github.com>
2023-03-05 11:49:56 +0200

    Update funkcijos.h

--- funkcijos.h
+++ funkcijos.h
@@ -3,6 +3,7 @@
 #define FUNKCIJOS_H_
 #include <vector>
 #include <string>
+#include <chrono>
 
 struct studentas {
     std::string vardas;
@@ -13,13 +14,43 @@ struct studentas {
     float galutinis_med {};
 };
 
+class Timer 
+{
+public:
+    Timer() {}
 
-void print(std::vector<studentas> &studentai);
+    void start() 
+    {
+        start_time_ = std::chrono::high_resolution_clock::now();
+    }
+
+    void stop() 
+    {
+        end_time_ = std::chrono::high_resolution_clock::now();
+    }
+
+    double elapsed() const 
+    {
+        return std::chrono::duration_cast<std::chrono::microseconds>(end_time_ - start_time_).count() / 1000000.0;
+    }
+
+private:
+    std::chrono::time_point<std::chrono::high_resolution_clock> start_time_;
+    std::chrono::time_point<std::chrono::high_resolution_clock> end_time_;
+};
+
+void isvedimas(std::vector<studentas> &studentai, std::string failo_pav);
 
 double count_med(std::vector<int> &nd_vec);
 
 double count_vid(const std::vector<int> &nd_vec);
 
 bool compare(const studentas& s1, const studentas& s2);
+
+std::string generuoti(int count);
+
+void rusiavimas(std::vector<studentas> &studentai, std::vector<studentas> &vargsai, std::vector<studentas> &malaciai);
+
+void nuskaitymas(std::vector<studentas> &studentai, std::string failo_pavadinimas);
 
 #endif
