075164463039c42a38f0af30636df1f17953c0ea
roxerj <32255747+roxerj@users.noreply.github.com>
2023-04-23 19:09:58 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,129 +7,90 @@
 #include <stdexcept>
 #include <filesystem>
 #include <limits>
+#include <list>
+#include <deque>
+
+char sub_choice;
 
 int main(){
     
-    int choice, studentu_skaicius;
-    std::string testas, failo_pavadinimas;
+    std::string failo_pavadinimas;
 
+    failo_pavadinimas = generavimas();
     
-    while (true) 
-    {
-        std::cout << "Iveskite 1, jei norite generuoti studentu faila, iveskite 2, jei norite atlikti veiksmus su esamu failu, arba 3, jei norite iseiti: ";
-        std::cin >> choice;
-
-        try 
-        {
-            if (std::cin.fail()) 
-            {
-                throw std::runtime_error("Klaida: blogas pasirinkimas. Pasirinkite 1, 2 arba 3");
-            }
-
-            if (choice == 1) 
-            {
-                std::cout << "Kiek bus studentu? ";
-                std::cin >> studentu_skaicius;
-
-                try {
-                    if (std::cin.fail() || studentu_skaicius <= 0) 
-                    {
-                        throw std::runtime_error("Klaida: neteisingai ivestas studentu skaicius");
-                    }
-
-                    testas = generuoti(studentu_skaicius);
-                    failo_pavadinimas = "stud" + std::to_string(studentu_skaicius) + ".txt";
-
-                    std::ofstream out_file(failo_pavadinimas);
-
-                    if (!out_file.is_open()) 
-                    {
-                        throw std::runtime_error("Klaida: nepavyko sukurti failo");
-                    }
-
-                    out_file << testas;
-                    out_file.close();
-                }
-                catch (const std::runtime_error& e) 
-                {
-                    std::cout << e.what() << std::endl;
-                    std::cin.clear();
-                    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-                }
-            }
-
-            else if (choice == 2) 
-            {
-                std::cout << "Esami failai:\n";
-
-                for (const auto& entry : std::filesystem::directory_iterator(".")) {
-                    if (entry.is_regular_file()) {
-                        std::cout << entry.path().filename() << std::endl;
-                    }
-                }
-
-
-                std::cout << "Iveskite failo pavadinima: ";
-                std::cin >> failo_pavadinimas;
-
-                std::ifstream in_file(failo_pavadinimas);
-
-                if (!in_file) 
-                {
-                    throw std::runtime_error("Klaida: failas " + failo_pavadinimas + " neegzistuoja");
-                }
-
-                in_file.close();
-                break;
-            }
-
-            else if (choice == 3) 
-            {
-                std::cout << "Programa baige darba" << std::endl;
-                return 0;
-            }
-
-            else 
-            {
-                throw std::runtime_error("Klaida: blogas pasirinkimas. Pasirinkite 1, 2 arba 3");
-            }
-        }
-        catch (const std::runtime_error& e) 
-        {
-            std::cout << e.what() << std::endl;
-            std::cin.clear();
-            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-        }
-    }
-
+    std::vector<studentas> studentai_vector;
+    std::vector<studentas> malaciai_vector;
+    std::vector<studentas> vargsai_vector;
     
-    
+    std::list<studentas> studentai_list;
+    std::list<studentas> malaciai_list;
+    std::list<studentas> vargsai_list;
+
+    std::deque<studentas> studentai_deque;
+    std::deque<studentas> malaciai_deque;
+    std::deque<studentas> vargsai_deque;
+
+
+
     Timer bendras_timer;
     bendras_timer.start();
-
-    std::vector<studentas> studentai;
     
     Timer nuskaitymas_timer;
     nuskaitymas_timer.start();
-    nuskaitymas(studentai, failo_pavadinimas);
+    if(sub_choice == '1')
+    {
+        nuskaitymas(studentai_vector, failo_pavadinimas);
+    }
+    else if(sub_choice == '2')
+    {
+        nuskaitymas(studentai_list, failo_pavadinimas);
+    }
+    else if(sub_choice == '3')
+    {
+        nuskaitymas(studentai_deque, failo_pavadinimas);
+    }
+
+    
     nuskaitymas_timer.stop();
     std::cout << "Duomenu nuskaitymas ir galutiniu pazymiu skaiciavimas uztruko " << nuskaitymas_timer.elapsed() << " sec." << std::endl;
 
     Timer sort_timer;
     sort_timer.start();
-    std::vector<studentas> vargsai;
-    std::vector<studentas> malaciai;
-    rusiavimas(studentai, vargsai, malaciai);
+    
+    if(sub_choice == '1')
+    {
+        rusiavimas(studentai_vector, vargsai_vector, malaciai_vector);
+    }
+    else if(sub_choice == '2')
+    {
+        rusiavimas(studentai_list, vargsai_list, malaciai_list);
+    }
+    else if(sub_choice == '3')
+    {
+        rusiavimas(studentai_deque, vargsai_deque, malaciai_deque);
+    }
     sort_timer.stop();
     std::cout << "Studentu rusiavimas i vargsus ir malacius uztruko " << sort_timer.elapsed() << " sec." << std::endl;
     
-
-    isvedimas(vargsai, "vargsai.txt");
-    isvedimas(malaciai, "malaciai.txt");
+    if(sub_choice == '1')
+    {
+        isvedimas<std::vector<studentas>>(vargsai_vector, "vargsai.txt");
+        isvedimas<std::vector<studentas>>(malaciai_vector, "malaciai.txt");
+    }
+    else if(sub_choice == '2')
+    {
+        isvedimas<std::list<studentas>>(vargsai_list, "vargsai.txt");
+        isvedimas<std::list<studentas>>(malaciai_list, "malaciai.txt");
+    }
+    else if(sub_choice == '3')
+    {
+        isvedimas<std::deque<studentas>>(vargsai_deque, "vargsai.txt");
+        isvedimas<std::deque<studentas>>(malaciai_deque, "malaciai.txt");
+    }
 
     bendras_timer.stop();
     std::cout << "Visos programos veikimo laikas " << bendras_timer.elapsed() << " sec." << std::endl;
-    std::cout << "Malaciu skaicius: " << malaciai.size() << ", vargsu skaicius: " << vargsai.size();
+    
 
     
 
