62087cdfed70340348579a4a20bc8af9731ca359
roxerj <juozapas.c@gmail.com>
2023-05-19 19:40:04 +0300

    removed a few files and added them to gitignore

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/juoza/cpp projects/UNI2
-# Build directory: C:/Users/juoza/cpp projects/UNI2/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-include("C:/Users/juoza/cpp projects/UNI2/build/hello_test[1]_include.cmake")
-subdirs("_deps/googletest-build")
